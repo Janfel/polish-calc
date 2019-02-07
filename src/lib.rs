cd9af3fce596ecdd5abab3eebb70c95d1ae4e431
Jan Felix Langenbach <o.hase3@gmail.com>
2019-02-07 18:32:15 +0100

    Add test suite.

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,12 @@
  * along with this program.  If not, see <http: //www.gnu.org/licenses/>.
  */
 
-pub fn run(arg: String) -> f32 {
+pub fn run(arg: String) -> f64 {
     let exprs: Vec<&str> = arg.split_whitespace().collect();
     compute(&exprs)[0]
 }
 
-fn compute(exprs: &[&str]) -> Vec<f32> {
+fn compute(exprs: &[&str]) -> Vec<f64> {
     if exprs.is_empty() {
         return Vec::new();
     }
@@ -41,4 +41,47 @@ fn compute(exprs: &[&str]) -> Vec<f32> {
     };
     vals.push(result);
     vals
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn calculate(expr: &str, expected: f64) {
+        let result = run(expr.to_owned());
+        assert!((result - expected).abs() < std::f64::EPSILON);
+    }
+
+    #[test]
+    fn addition() {
+        calculate("+ 2 3", 5.0)
+    }
+
+    #[test]
+    fn subtraction() {
+        calculate("- 10 5", 5.0)
+    }
+
+    #[test]
+    fn multiplication() {
+        calculate("* 3 3", 9.0)
+    }
+
+    #[test]
+    fn division() {
+        calculate("/ 6 2", 3.0)
+    }
+
+    #[test]
+    fn real_division() {
+        calculate("/ 2 3", 2.0 / 3.0)
+    }
+
+    #[allow(clippy::float_cmp)]
+    #[test]
+    fn zero_division() {
+        let result = run("/ 6 0".to_owned());
+        assert_eq!(result, std::f64::INFINITY);
+    }
+
 }
