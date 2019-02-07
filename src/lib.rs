685d8578408905252a14e2a678c33c7fb4d5ba0d
Jan Felix Langenbach <o.hase3@gmail.com>
2019-02-07 17:25:36 +0100

    Add logic.

--- src/lib.rs
+++ src/lib.rs
@@ -16,13 +16,29 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http: //www.gnu.org/licenses/>.
  */
-use std::env;
 
-use plc::run;
+pub fn run(arg: String) -> f32 {
+    let exprs: Vec<&str> = arg.split_whitespace().collect();
+    compute(&exprs)[0]
+}
 
-fn main() {
-    let arg = env::args().nth(1).expect("No input");
-    println!("{:?}", arg);
-    let result = run(arg);
-    println!("{}", result);
+fn compute(exprs: &[&str]) -> Vec<f32> {
+    if exprs.is_empty() {
+        return Vec::new();
+    }
+    if exprs.len() == 1 {
+        return vec![exprs[0].parse().expect("Invalid Token")];
+    }
+    let mut vals = compute(&exprs[1..]);
+    let mut getval = || vals.pop().expect("Not enough operands in expression");
+
+    let result = match exprs[0] {
+        "+" => getval() + getval(),
+        "-" => getval() - getval(),
+        "*" => getval() * getval(),
+        "/" => getval() / getval(),
+        num => num.parse().expect("Invalid Token"),
+    };
+    vals.push(result);
+    vals
 }
